549010dcfa80a712b3770c8780eef1398ab386b4
月岡陽人 <87177693+otsukky@users.noreply.github.com>
2024-08-16 21:38:24 +0900

    小数点以下を切り捨てない

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -3,5 +3,5 @@ package org.example
 class Calculator {
     fun multiply(x: Int, y: Int): Int = x * y
 
-    fun divide(x: Int, y: Int): Int = x / y
+    fun divide(x: Int, y: Int): Float = x.toFloat() / y.toFloat()
 }
