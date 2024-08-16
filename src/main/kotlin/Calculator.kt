1008e9731b587238ebffbff8013abf0a6b1aedf8
月岡陽人 <87177693+otsukky@users.noreply.github.com>
2024-08-16 21:40:36 +0900

    ゼロ除算は不可

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -3,5 +3,10 @@ package org.example
 class Calculator {
     fun multiply(x: Int, y: Int): Int = x * y
 
-    fun divide(x: Int, y: Int): Float = x.toFloat() / y.toFloat()
+    fun divide(x: Int, y: Int): Float =
+        if (y == 0) {
+            throw IllegalArgumentException("divide by zero")
+        } else {
+            x.toFloat() / y.toFloat()
+        }
 }
